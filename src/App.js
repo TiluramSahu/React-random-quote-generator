3095142c4780b251a717d32e46683ff6eb937c31
DJ_Tillu <djtillu@gmail.com>
2023-06-30 23:13:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [quote, setQuote] = useState(" ");
+  const [author, setAuthor] = useState(" ");
+  //   http://api.quotable.io/random
+ 
+
+  useEffect(()=>{
+      fetch("http://api.quotable.io/random")
+      .then(res => res.json())
+      .then(
+        (quote) => {
+          setQuote(quote.content);
+          setAuthor(quote.author);
+        }
+      )
+  },[]);
+
+
+  let fetchNewQuote = () => {
+    fetch("http://api.quotable.io/random")
+    .then(res => res.json())
+    .then(
+      (quote) => {
+        setQuote(quote.content);
+        setAuthor(quote.author);
+      }
+    )
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className='App'>
+   <div className="heading"><h1>React Random Quote Generator</h1></div>
+    <div className="quote">
+      <h2>❝{quote}<sub>❞</sub></h2>
+      <small>- {author} -</small>
     </div>
+    <br/>
+    <button class="button-7" role="button" onClick={fetchNewQuote}>Next Quote </button>
+    <footer class="footer">
+  <p class="footer-text">Made with ❤️ by <h2>Tiluram Sahu</h2></p>
+</footer>
+   </div>
   );
 }
 
