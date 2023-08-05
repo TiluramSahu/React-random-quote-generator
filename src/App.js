627660714a9ce60e7ad4e895a1e355276f327bc3
DJ_Tillu <djtillu@gmail.com>
2023-08-05 23:05:37 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -2,44 +2,32 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
-  const [quote, setQuote] = useState(" ");
-  const [author, setAuthor] = useState(" ");
-  //   http://api.quotable.io/random
- 
+  const [quote, setQuote] = useState("");
+  const getQuote = () => {
+    fetch("https://type.fit/api/quotes")
+    .then(res => res.json())
+    .then((data) => {
+        let randomNum = Math.floor(Math.random() * data.length);
+        setQuote(data[randomNum]);
+    })
+  }
 
   useEffect(()=>{
-      fetch("http://api.quotable.io/random")
-      .then(res => res.json())
-      .then(
-        (quote) => {
-          setQuote(quote.content);
-          setAuthor(quote.author);
-        }
-      )
-  },[]);
+    getQuote();
+  },[])
 
 
-  let fetchNewQuote = () => {
-    fetch("http://api.quotable.io/random")
-    .then(res => res.json())
-    .then(
-      (quote) => {
-        setQuote(quote.content);
-        setAuthor(quote.author);
-      }
-    )
-  }
 
 
   return (
    <div className='App'>
    <div className="heading"><h1>React Random Quote Generator</h1></div>
     <div className="quote">
-      <h2>❝{quote}<sub>❞</sub></h2>
-      <small>- {author} -</small>
+      <h2>❝{quote.text}<sub>❞</sub></h2>
+      <small>- {quote.author} -</small>
     </div>
     <br/>
-    <button class="button-7" role="button" onClick={fetchNewQuote}>Next Quote </button>
+    <button class="button-7" role="button" onClick={getQuote}>Next Quote </button>
     <footer class="footer">
   <p class="footer-text">Made with ❤️ by <h2>Tiluram Sahu</h2></p>
 </footer>
